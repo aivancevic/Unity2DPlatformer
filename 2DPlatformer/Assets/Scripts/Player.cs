4904d5010d62c8476e0f3880ee1e80f067c5339c
aivancevic <65815501+aivancevic@users.noreply.github.com>
2022-03-09 22:58:59 +0100

    Added player animations
    Added idle and walking animations.

--- 2DPlatformer/Assets/Scripts/Player.cs
+++ 2DPlatformer/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
     private float horizontalInput = 0.0f;
     [SerializeField] private float speed = 4.0f;
 
@@ -16,6 +17,15 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Script: Player\t Rigidbody 2D is NULL");
+        }
+        anim = GetComponent<Animator>();
+        if (anim == null)
+        {
+            Debug.LogError("Script: Player\t Animator is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +39,18 @@ public class Player : MonoBehaviour
     private void Move()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
     }
 
